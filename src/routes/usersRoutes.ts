2db3afc59f51755f87d2a0b4c3a30bcfe4152649
EduardooPV <eduardoveltroni@hotmail.com>
2024-02-21 22:19:27 -0300

    feat: create delete user route

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.route("/users").post(userController.createUser);
 
-router.route("/users/:userId").put(userController.editUser);
+router.route("/users/:userId").put(userController.editUser).delete(userController.deleteUser)
 
 export default router;
