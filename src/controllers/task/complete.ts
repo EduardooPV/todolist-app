655252714866cb903948655b23a0be201030f7b0
EduardooPV <eduardoveltroni@hotmail.com>
2024-03-09 20:40:18 -0300

    feat: toggle task

---
+++ src/controllers/task/complete.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import TaskModel from "../../models/task";
+
+import { ZodError, z } from "zod";
+
+const complete = async (req: Request, res: Response) => {
+  const taskParams = z.object({
+    taskId: z.string(),
+  });
+
+  try {
+    const { taskId } = taskParams.parse(req.params);
+
+    const taskAlreadyExist = await TaskModel.findOne({
+      _id: taskId,
+    });
+
+    if (!taskAlreadyExist) {
+      return res.status(400).json({
+        error: true,
+        message: "Tarefa não encontrada, verifique o id.",
+      });
+    }
+
+    const filter = { _id: taskId };
+    const update = { status: !taskAlreadyExist.status };
+    const options = { new: true };
+
+    const updateTask = await TaskModel.findOneAndUpdate(
+      filter,
+      update,
+      options
+    );
+
+    return res.status(201).json({
+      error: false,
+      message: `Tarefa alterada para ${
+        updateTask?.status ? "concluida" : "em andamento"
+      }.`,
+      updateTask,
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      console.log(error);
+      return res.status(500).json({
+        error: true,
+        message: "Erro de validação, tente novamente.",
+        details: error.errors.map((err) => err.message),
+      });
+    } else {
+      return res.status(500).json({
+        error: true,
+        message:
+          "Não foi possível se conectar ao servidor, tente novamente mais tarde.",
+      });
+    }
+  }
+};
+
+export default complete;
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 import createTask from "../controllers/task/create";
 import getAll from "../controllers/task/getAll";
+import complete from "../controllers/task/complete";
 
 const router = express.Router();
 
 router.route("/task").post(createTask);
 router.route("/task/:userId").get(getAll)
+router.route("/task/toggle/:taskId").patch(complete)
 
 export default router;
