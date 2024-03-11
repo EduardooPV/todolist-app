1a3f1e113d35e3c831c2062d57fb40d7c4b030a2
EduardooPV <eduardoveltroni@hotmail.com>
2024-03-10 22:22:15 -0300

    feat: delete task

---
+++ src/controllers/task/delete.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import TaskModel from "../../models/task";
+
+import { ZodError, z } from "zod";
+
+const deleteTask = async (req: Request, res: Response) => {
+  const taskParams = z.object({
+    taskId: z.string(),
+  });
+
+  try {
+    const { taskId } = taskParams.parse(req.params);
+
+    const deletedTask = await TaskModel.deleteOne({
+      _id: taskId,
+    });
+
+    if (deletedTask.deletedCount === 0) {
+      return res.status(400).json({
+        error: true,
+        message: "Tarefa não encontrada ou já excluida.",
+      });
+    }
+
+    return res.status(201).json({
+      error: false,
+      message: "Tarefa excluida com sucesso.",
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      console.log(error);
+      return res.status(500).json({
+        error: true,
+        message: "Erro de validação, tente novamente.",
+        details: error.errors.map((err) => err.message),
+      });
+    } else {
+      return res.status(500).json({
+        error: true,
+        message:
+          "Não foi possível se conectar ao servidor, tente novamente mais tarde.",
+      });
+    }
+  }
+};
+
+export default deleteTask;
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -4,12 +4,13 @@ import createTask from "../controllers/task/create";
 import getAll from "../controllers/task/getAll";
 import complete from "../controllers/task/complete";
 import update from "../controllers/task/update";
+import deleteTask from "../controllers/task/delete";
 
 const router = express.Router();
 
 router.route("/task").post(createTask);
 router.route("/task/:userId").get(getAll);
 router.route("/task/toggle/:taskId").patch(complete);
-router.route("/task/:taskId").put(update);
+router.route("/task/:taskId").put(update).delete(deleteTask);
 
 export default router;
